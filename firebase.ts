0ca87882c492c89702000da40182e2e12f33e311
rasuwebdev <apiwadaphysics.lk@gmail.com>
2026-02-01 23:16:32 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD_hDpsNq60hwsEYGIAWFkn7LgLYPoD2D8",
   authDomain: "apiwada.firebaseapp.com",
@@ -11,5 +16,6 @@ const firebaseConfig = {
   measurementId: "G-71ZETYBFM0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
