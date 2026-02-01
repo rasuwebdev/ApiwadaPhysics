1f9bf0de1a850d6f8317db7525098fb1bf298f2d
rasuwebdev <apiwadaphysics.lk@gmail.com>
2026-02-01 23:15:21 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.tsx';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Dashboard from './components/Dashboard';
